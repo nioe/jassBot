0c4e271c20a181c2aec026b6568116d46cc6a63a
Nicolas Oeschger <noe@exq.ch>
2015-10-15 22:35:48 +0200

    Implemented dumb choose trumpf logic

--- index.js
+++ index.js
@@ -1,29 +1,60 @@
 'use strict';
- 
+
 let Bot = require('javascript-jass-bot');
- 
+
 class BotStrategy {
     requestTrumpf(cards) {
-        // e.g. choose TRUMPF SPADES
+        let amountPerColor = {
+            HEARTS: 0,
+            SPADES: 0,
+            CLUBS: 0,
+            DIAMONDS: 0
+        };
+
+        cards.forEach((card) => {
+            amountPerColor[card.color]++;
+
+            switch (card.number) {
+                case 9: // Nell
+                    amountPerColor[card.color]++;
+                    break;
+                case 11: // Buur
+                    amountPerColor[card.color] += 2;
+                    break;
+            }
+        });
+
+        let max = 0, trumpf;
+        for (let color in amountPerColor) {
+            if (amountPerColor[color] > max) {
+                max = amountPerColor[color];
+                trumpf = color;
+            }
+        }
+
+        console.log('cards', cards);
+        console.log('amountPerColor', amountPerColor);
+        console.log('trumpf', trumpf);
+
         let response = {};
         response.mode = 'TRUMPF';
-        response.trumpfColor = 'SPADES';
+        response.trumpfColor = trumpf;
         return response;
     }
-    
+
     playCard(myCards, playedCards, gameState) {
         // e.g. play random
-        return myCards[Math.floor(Math.random()*myCards.length)];
+        return myCards[Math.floor(Math.random() * myCards.length)];
     }
-    
-    
+
+
     gameFinished(data) {
         console.log(data);
     }
- 
+
     notifyError(error) {
         console.log(error);
     }
 }
- 
-new Bot('ChuckNorris').withStrategy(new BotStrategy()).connect('localhost:3000');
+
+new Bot(process.argv[2]).withStrategy(new BotStrategy()).connect(process.argv[3] + ':3000');
